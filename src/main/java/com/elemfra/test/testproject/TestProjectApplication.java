41d0f7d3c390247b9e23963ce3769d66c85351c9
elemfra <francesco.lemma@ericsson.com>
2017-11-08 06:51:29 -1000

    Testing SonarQube... Introducing issues in the code

--- src/main/java/com/elemfra/test/testproject/TestProjectApplication.java
+++ src/main/java/com/elemfra/test/testproject/TestProjectApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class TestProjectApplication {
@@ -21,5 +23,13 @@ class HelloWorldRestController {
     @RequestMapping("/hello/{name}")
     public String hello(@PathVariable(value="name") String name) {
         return String.format("Hello %s!!", name);
+    }
+
+    private void sonarQubeIssues() {
+        //TODO: Testing SonarQube
+        List<String> l = new ArrayList<String>();
+
+        String nullString = null;
+        nullString.equals("My_Sweet_String");
     }
 }
