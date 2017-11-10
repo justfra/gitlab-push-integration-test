ca9cc796a49701e045d73fb2d68a1cb3f7275433
elemfra <francesco.lemma@ericsson.com>
2017-11-10 01:10:46 -1000

    Testing SonarQube... Introducing issues in the code

--- src/main/java/com/elemfra/test/testproject/TestProjectApplication.java
+++ src/main/java/com/elemfra/test/testproject/TestProjectApplication.java
@@ -20,34 +20,6 @@ public class TestProjectApplication {
 
 @RestController
 class HelloWorldRestController {
-
-/*
-    private void sonarQubeFail() {
-        //TODO: Testing SonarQube
-        List<String> l = new ArrayList<String>();
-
-        String nullString = null;
-        nullString.equals("My_Sweet_String");
-
-        Object o = null;
-        o.equals(nullString);
-    }
-*/
-
-    private void testCode() {
-        if (true) {
-            if (!false) {
-                if (true) {
-                    if (!false) {
-                        if (true) {
-                            System.out.println(".......");
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     @RequestMapping("/hello/{name}")
     public String hello(@PathVariable(value="name") String name) {
         return String.format("Hello %s!!", name);
